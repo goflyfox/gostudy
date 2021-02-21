572f0835f7680b8ac6227a7ef5822faa57a86e22
jflyfox <zcool321@sina.com>
2021-02-21 22:31:27 +0800

    update doc

--- 09.ptrAndStruct/03methods.go
+++ 09.ptrAndStruct/03methods.go
@@ -2,21 +2,21 @@ package main
 
 import "fmt"
 
-type rect struct {
+type rectangle struct {
 	width, height int
 }
 
 // 这里的 area 方法有一个接收器类型 rect。
-func (r *rect) area() int {
+func (r *rectangle) area() int {
 	return r.width * r.height
 }
 
 // 可以为值类型或者指针类型的接收器定义方法。这里是一个值类型接收器的例子。
-func (r rect) perim() int {
+func (r rectangle) perim() int {
 	return 2*r.width + 2*r.height
 }
 func main() {
-	r := rect{width: 10, height: 5}
+	r := rectangle{width: 10, height: 5}
 	// 这里我们调用上面为结构体定义的两个方法。
 	fmt.Println("area: ", r.area())
 	fmt.Println("perim:", r.perim())
